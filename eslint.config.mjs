a2480c9d0a07f5038d9095b1adfcfc38acd4643e
DavidCs9 <davidcastrosi@hotmail.com>
2025-06-11 19:40:01 +0000

    feat: enhance ESLint configuration with React support and update rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,21 +2,28 @@ import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
-import { defineConfig } from 'eslint/config';
 
-export default defineConfig([
+export default [
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    rules: {
-      'react/react-in-jsx-scope': 'off',
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 2020,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    languageOptions: { globals: globals.browser },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-no-target-blank': 'warn',
+    },
   },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+];
